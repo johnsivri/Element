16fad8f24a1022537b1216a56231e07b3fb178b2
John Sivri <jksivri@gmail.com>
2018-07-29 16:20:52 -0400

    Re-Initialization

---
+++ Source/Element/ElementGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ElementGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELEMENT_API AElementGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
